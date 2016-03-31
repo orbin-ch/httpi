98de3d8784c53fd7c8a16d5f4fb99feb80d11e1f
dnii <dniisomething@gmail.com>
2016-03-31 21:30:22 +0200

    Prepare release 0.3.0

--- dist/httpi.js
+++ dist/httpi.js
@@ -66,6 +66,10 @@ return /******/ (function(modules) { // webpackBootstrap
 
 	var _response2 = _interopRequireDefault(_response);
 
+	var _headers = __webpack_require__(2);
+
+	var _headers2 = _interopRequireDefault(_headers);
+
 	function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
 	function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
@@ -76,7 +80,7 @@ return /******/ (function(modules) { // webpackBootstrap
 
 	    options = options || {};
 	    this.baseUrl = options.baseUrl || "";
-	    this.headers = options.headers || {};
+	    this.headers = new _headers2.default(options.headers || {});
 	    this.interceptors = [];
 	  }
 
@@ -88,8 +92,9 @@ return /******/ (function(modules) { // webpackBootstrap
 	      var promise = new Promise(function (resolve, reject) {
 	        var xhr = new window.XMLHttpRequest();
 
-	        xhr.onload = function (e) {
-	          var response = new _response2.default(e);
+	        xhr.onload = function () {
+	          var response = new _response2.default(this);
+
 	          if (response.isSuccess) {
 	            resolve(response);
 	          } else {
@@ -97,65 +102,48 @@ return /******/ (function(modules) { // webpackBootstrap
 	          }
 	        };
 
-	        xhr.onerror = function (e) {
-	          reject(new _response2.default(e));
+	        xhr.onerror = function () {
+	          reject(new _response2.default(this));
 	        };
 
-	        xhr.ontimeout = function (e) {
-	          reject(new _response2.default(e));
+	        xhr.ontimeout = function () {
+	          reject(new _response2.default(this));
 	        };
 
 	        xhr.open(options.method, _this.baseUrl + options.url, true);
 
-	        var _iteratorNormalCompletion = true;
-	        var _didIteratorError = false;
-	        var _iteratorError = undefined;
+	        _this.headers.forEach(function (value, name) {
+	          xhr.setRequestHeader(name, value);
+	        });
 
-	        try {
-	          for (var _iterator = Object.keys(_this.headers)[Symbol.iterator](), _step; !(_iteratorNormalCompletion = (_step = _iterator.next()).done); _iteratorNormalCompletion = true) {
-	            var header = _step.value;
-
-	            xhr.setRequestHeader(header, _this.headers[header]);
-	          }
-	        } catch (err) {
-	          _didIteratorError = true;
-	          _iteratorError = err;
-	        } finally {
-	          try {
-	            if (!_iteratorNormalCompletion && _iterator.return) {
-	              _iterator.return();
-	            }
-	          } finally {
-	            if (_didIteratorError) {
-	              throw _iteratorError;
-	            }
-	          }
+	        if (!_this.headers.has("Content-Type")) {
+	          xhr.setRequestHeader("Content-Type", "application/json");
 	        }
 
-	        xhr.send(options.body);
+	        xhr.send(options.body ? JSON.stringify(options.body) : null);
 	      });
 
-	      var _iteratorNormalCompletion2 = true;
-	      var _didIteratorError2 = false;
-	      var _iteratorError2 = undefined;
+	      var _iteratorNormalCompletion = true;
+	      var _didIteratorError = false;
+	      var _iteratorError = undefined;
 
 	      try {
-	        for (var _iterator2 = this.interceptors[Symbol.iterator](), _step2; !(_iteratorNormalCompletion2 = (_step2 = _iterator2.next()).done); _iteratorNormalCompletion2 = true) {
-	          var interceptor = _step2.value;
+	        for (var _iterator = this.interceptors[Symbol.iterator](), _step; !(_iteratorNormalCompletion = (_step = _iterator.next()).done); _iteratorNormalCompletion = true) {
+	          var interceptor = _step.value;
 
 	          promise.then(interceptor.response, interceptor.responseError);
 	        }
 	      } catch (err) {
-	        _didIteratorError2 = true;
-	        _iteratorError2 = err;
+	        _didIteratorError = true;
+	        _iteratorError = err;
 	      } finally {
 	        try {
-	          if (!_iteratorNormalCompletion2 && _iterator2.return) {
-	            _iterator2.return();
+	          if (!_iteratorNormalCompletion && _iterator.return) {
+	            _iterator.return();
 	          }
 	        } finally {
-	          if (_didIteratorError2) {
-	            throw _iteratorError2;
+	          if (_didIteratorError) {
+	            throw _iteratorError;
 	          }
 	        }
 	      }
@@ -212,6 +200,71 @@ return /******/ (function(modules) { // webpackBootstrap
 	}();
 
 	exports.default = Response;
+
+/***/ },
+/* 2 */
+/***/ function(module, exports) {
+
+	"use strict";
+
+	Object.defineProperty(exports, "__esModule", {
+	  value: true
+	});
+
+	var _createClass = function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; }();
+
+	function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
+
+	var Headers = function () {
+	  function Headers(headers) {
+	    _classCallCheck(this, Headers);
+
+	    this.headers = headers;
+	  }
+
+	  _createClass(Headers, [{
+	    key: "has",
+	    value: function has(name) {
+	      name = name.toLowerCase();
+
+	      return Object.keys(this.headers).some(function (key) {
+	        return key.toLowerCase() === name;
+	      });
+	    }
+	  }, {
+	    key: "forEach",
+	    value: function forEach(callback) {
+	      var _iteratorNormalCompletion = true;
+	      var _didIteratorError = false;
+	      var _iteratorError = undefined;
+
+	      try {
+	        for (var _iterator = Object.keys(this.headers)[Symbol.iterator](), _step; !(_iteratorNormalCompletion = (_step = _iterator.next()).done); _iteratorNormalCompletion = true) {
+	          var header = _step.value;
+
+	          callback(this.headers[header], header);
+	        }
+	      } catch (err) {
+	        _didIteratorError = true;
+	        _iteratorError = err;
+	      } finally {
+	        try {
+	          if (!_iteratorNormalCompletion && _iterator.return) {
+	            _iterator.return();
+	          }
+	        } finally {
+	          if (_didIteratorError) {
+	            throw _iteratorError;
+	          }
+	        }
+	      }
+	    }
+	  }]);
+
+	  return Headers;
+	}();
+
+	exports.default = Headers;
 
 /***/ }
 /******/ ])
